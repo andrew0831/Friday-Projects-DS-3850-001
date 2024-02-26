846125a244699c89e4c0666e84eab8947da7c89b
andrew0831 <161264737+andrew0831@users.noreply.github.com>
2024-02-25 21:22:41 -0600

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,14 @@
+large_object = input("Please provide a large object: ")
+large_objects_plural = input("Please provide large objects (plural): ")
+adjective = input("Please provide an adjective: ")
+body_part = input("Please provide a body part: ")
+restaurant = input("Please provide a restaurant: ")
+first_food = input("Please provide a food: ")
+second_food = input("Please provide another food: ")
+
+# Printing the story with the user-provided words
+print("I’ve had a very", adjective, "day.")
+print("This morning, I dropped a box of", large_objects_plural, "on my", body_part + ".")
+print("Then, at lunch, I went to", restaurant, "for their delicious", first_food + ",")
+print("But the waiter brought me", second_food + ", which I was not hungry for.")
+print("Finally, on my way home, I was cut off by a van with a large", large_object, "strapped to the roof.")
